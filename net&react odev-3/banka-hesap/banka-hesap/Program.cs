d297a5149e8a7cdd356ed396c3c33143e8a970b6
Süleyman Tuncer <suleymantuncer061@gmail.com>
2025-03-09 18:29:12 +0300

    güncel

---
+++ net&react odev-3/banka-hesap/banka-hesap/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Globalization;
+
+namespace BankaHesap
+{
+    public class BankAccount
+    {
+        public string AccountHolder { get; set; }
+        public decimal Balance { get; set; }
+
+        public BankAccount(string accountHolder, decimal balance)
+        {
+            AccountHolder = accountHolder;
+            Balance = balance;
+        }
+
+        public virtual void CalculateInterest()
+        {
+            
+        }
+    }
+
+    public class SavingsAccount : BankAccount
+    {
+        public SavingsAccount(string accountHolder, decimal balance)
+            : base(accountHolder, balance)
+        {
+        }
+
+        public override void CalculateInterest()
+        {
+            decimal interest = Balance * 0.3m;
+            CultureInfo trCulture = new CultureInfo("tr-TR");
+            trCulture.NumberFormat.CurrencySymbol = "₺";
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine($"Vadeli hesap faizi: {interest.ToString("C2", trCulture)}");
+        }
+    }
+
+    public class CheckingAccount : BankAccount
+    {
+        public CheckingAccount(string accountHolder, decimal balance)
+            : base(accountHolder, balance)
+        {
+        }
+
+        public override void CalculateInterest()
+        {
+            Console.WriteLine("Checking accounts do not earn interest.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            SavingsAccount vadeliHesap = new SavingsAccount("Süleyman Mert Tuncer", 4000m);
+            vadeliHesap.CalculateInterest();
+
+            CheckingAccount vadesizHesap = new CheckingAccount("Mert Tuncer", 1000m);
+            vadesizHesap.CalculateInterest();
+        }
+    }
+}
---
+++ net&react odev-3/calisan-sinif/calisan-sinif/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace CalisanYonetimi
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+        public string Department { get; set; }
+
+        public virtual decimal CalculateBonus()
+        {
+            // Temel uygulama (varsa)
+            return 0;
+        }
+    }
+
+    public class Manager : Employee
+    {
+        public int TeamSize { get; set; }
+
+        public override decimal CalculateBonus()
+        {
+            // Yönetici primi
+            return Salary * 20m;
+        }
+    }
+
+    public class Developer : Employee
+    {
+        public string About { get; set; }
+
+        public override decimal CalculateBonus()
+        {
+            // Geliştiriciler primi
+            return Salary * 10m;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager manager = new Manager
+            {
+                Id = 1,
+                Name = "Suleyman Mert Tuncer",
+                Salary = 12500,
+                Department = "IT",
+                TeamSize = 25,
+            };
+
+            Developer developer = new Developer
+            {
+                Id = 2,
+                Name = "Ali",
+                Salary = 5000,
+                Department = "IT",
+                About = "Frontend Developer"
+            };
+
+            Console.WriteLine($"Yönetici Primi: {manager.CalculateBonus()}");
+            Console.WriteLine($"Geliştirici Primi: {developer.CalculateBonus()}");
+        }
+    }
+}
